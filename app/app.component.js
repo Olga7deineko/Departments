01c48a876d200041922ba0001c4237cca07eb35b
Olga7deineko <olga7deineko@gmail.com>
2016-10-20 23:01:43 +0300

    Add files via upload

--- app/app.component.js
+++ app/app.component.js
@@ -11,12 +11,13 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 var core_1 = require('@angular/core');
 var AppComponent = (function () {
     function AppComponent() {
+        this.title = 'Tour of Deprtaments';
     }
     AppComponent = __decorate([
         core_1.Component({
             moduleId: module.id,
             selector: 'my-app',
-            template: 'department-detail.component.html',
+            template: "\n    <h1>{{title}}</h1>\n    <nav>\n      <a routerLink=\"/departments\" routerLinkActive=\"active\">Departments</a>\n    </nav>\n    <router-outlet></router-outlet>\n  ",
             styleUrls: ['app.component.css'],
         }), 
         __metadata('design:paramtypes', [])
--- app/app.component.ts
+++ app/app.component.ts
@@ -3,11 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   moduleId: module.id,
   selector: 'my-app',
-  template: 'department-detail.component.html',
+  template: `
+    <h1>{{title}}</h1>
+    <nav>
+      <a routerLink="/departments" routerLinkActive="active">Departments</a>
+    </nav>
+    <router-outlet></router-outlet>
+  `,
   styleUrls: ['app.component.css'],
 })
 export class AppComponent {
-
+  title = 'Tour of Deprtaments';
 }
 
 
